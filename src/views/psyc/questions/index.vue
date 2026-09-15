45f2cab547b00bd54b427d5b3ea43501dcdedf60
suwenshi <1054611012@qq.com>
2026-09-15 11:09:24 +0800

    refactor: 移除前端 psyc 心理模块相关代码
    - 删除 src/api/psyc 下 4 个接口（feedback/post/questions/test）
    - 删除 src/views/psyc 下 6 个页面（测试/反馈/帖子/题目等）
    - 删除 src/components/PsycQuestion/QuestionPanel.vue 组件

--- src/views/psyc/questions/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <question-panel />
-</template>
-
-<script setup>
-import QuestionPanel from '@/components/PsycQuestion/QuestionPanel.vue'
-
-defineOptions({ name: "Questions" })
-</script>
--- src/views/psyc/test-questions/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <questions />
-</template>
-
-<script setup>
-import Questions from '@/views/psyc/test/questions.vue'
-</script>
